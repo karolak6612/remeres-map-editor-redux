14b6e8c8321a0d4544868b4e835654311a83a582
glaszczkoldre <karolak6612@gmail.com>
2026-02-17 20:27:05 +0100

    first iteration

--- source/ui/welcome_dialog.h
+++ source/ui/welcome_dialog.h
@@ -5,22 +5,26 @@
 #include <vector>
 
 // Forward declarations
-class ModernButton;
+class wxListCtrl;
+class wxImageList;
 
 wxDECLARE_EVENT(WELCOME_DIALOG_ACTION, wxCommandEvent);
 
 class WelcomeDialog : public wxDialog {
 public:
 	WelcomeDialog(const wxString& titleText, const wxString& versionText, const wxSize& size, const wxBitmap& rmeLogo, const std::vector<wxString>& recentFiles);
-
-	// Event Handlers
-	void OnButtonClicked(wxCommandEvent& event);
-	void OnCheckboxClicked(wxCommandEvent& event);
-	void OnRecentFileClicked(wxCommandEvent& event);
+	~WelcomeDialog();
 
 private:
-	// Implementation details
-	class WelcomePanel;
+	// Event Handlers
+	void OnButtonClicked(wxCommandEvent& event);
+	void OnRecentFileActivated(wxCommandEvent& event);
+	void OnRecentFileSelected(wxCommandEvent& event);
+
+	void AddInfoField(wxSizer* sizer, wxWindow* parent, const wxString& label, const wxString& value, const wxString& artId, const wxColour& valCol = wxColour(0, 0, 0));
+
+	wxImageList* m_imageList;
+	wxListCtrl* m_recentList;
 };
 
 #endif // WELCOME_DIALOG_H
